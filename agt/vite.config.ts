5a18c7c47f9afcea5bec7b8ce54be5725b3a6818
KrakishMusta <project.zero.one01@gmail.com>
2025-08-16 16:50:29 +0700

    Перенастройка gh-pages

--- agt/vite.config.ts
+++ agt/vite.config.ts
@@ -1,12 +1,10 @@
 import { fileURLToPath, URL } from 'node:url';
-
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/Art-Gallery/', // 👈 ОБЯЗАТЕЛЬНО для GitHub Pages
+  base: '/Art-Gallery/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
