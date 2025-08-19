46badb7d39fa52b849a194b71ef2d04ac28a686c
KrakishMusta <project.zero.one01@gmail.com>
2025-08-19 11:44:46 +0700

    Изменение настроек линтера и форматера

--- agt/.eslintrc.cjs
+++ agt/.eslintrc.cjs
@@ -1,4 +1,4 @@
-require('@rushstack/eslint-patch/modern-module-resolution')
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
@@ -10,18 +10,33 @@ module.exports = {
   extends: [
     '@vue/eslint-config-airbnb',
     '@vue/eslint-config-typescript',
-    'plugin:prettier/recommended',
+    'plugin:prettier/recommended', // включает prettier и интегрирует его в eslint
   ],
   rules: {
+    // Отключаем конфликты airbnb/prettier
+    'import/no-extraneous-dependencies': 'off',
+    'import/no-unresolved': 'off',
+
+    // Prettier правила, обязательные для проекта
     'prettier/prettier': [
       'error',
       {
-        semi: true,        // точки с запятой обязательны
-        tabWidth: 2,       // 2 пробела
+        tabWidth: 2,        // 2 пробела для отступов
         useTabs: false,
-        singleQuote: true, // одинарные кавычки (можно выключить)
-        trailingComma: 'es5'
+        semi: true,         // точки с запятой обязательны
+        singleQuote: true,
+        trailingComma: 'es5',
+        endOfLine: 'auto',  // избегает проблем с CRLF/LF
+        printWidth: 80
       },
     ],
   },
-}
+  overrides: [
+    {
+      files: ['*.vue'],
+      rules: {
+        'indent': 'off', // выключаем правило airbnb для отступов в vue
+      },
+    },
+  ],
+};
