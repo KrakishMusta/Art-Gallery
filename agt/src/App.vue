b406194c2bfc064d2463e7ac8cddbcce428244df
KrakishMusta <project.zero.one01@gmail.com>
2025-08-19 11:45:49 +0700

    Определены стили и темы приложения

--- agt/src/App.vue
+++ agt/src/App.vue
@@ -1,14 +1,11 @@
 <script setup lang="ts">
-const farr = 100;
+import MasterHead from './components/MasterHead.vue';
+import GalleryPage from './pages/GalleryPage.vue';
 </script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit
-    <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to
-    read the documentation
-  </p>
+  <MasterHead />
+  <GalleryPage />
 </template>
 
 <style scoped></style>
--- agt/src/main.ts
+++ agt/src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import App from './App.vue';
+import './styles/global.scss';
 
 const app = createApp(App);
 
